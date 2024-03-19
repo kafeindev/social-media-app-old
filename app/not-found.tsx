07528018aad2428d3d1094a4afdbeb01a376e6e4
KafeinDev <kafein40@gmail.com>
2024-03-19 03:00:10 +0300

    feat: add error and not-found pages

---
+++ app/not-found.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import { BackArrowIcon } from "./icons";
+
+export default function NotFound() {
+  return (
+    <div className="flex w-full flex-col items-center justify-center gap-2">
+      <h1 className="text-8xl font-bold text-secondary/30">Not Found</h1>
+      <Link
+        href="/"
+        className="border-lg flex h-12 w-48 items-center justify-center gap-2 rounded-lg text-lg font-semibold text-secondary-foreground/50 hover:bg-secondary/20"
+      >
+        {BackArrowIcon} Return Home
+      </Link>
+    </div>
+  );
+}
