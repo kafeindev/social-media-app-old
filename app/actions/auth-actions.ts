36aa24a7187ee0ada1079ea1cf2dd20537b18977
KafeinDev <kafein40@gmail.com>
2024-03-26 21:52:29 +0300

    refactor: made improvements to authentication files

--- app/actions/auth-actions.ts
+++ app/actions/auth-actions.ts
@@ -1,31 +1,32 @@
 "use server";
 
-import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
-
 import { UserSchemaType } from "@/lib/definitions";
 import { createClient } from "@/lib/supabase/server";
 
-export async function signin(schema: UserSchemaType) {
+export async function signin(schema: UserSchemaType): Promise<string | null> {
   const supabase = createClient();
 
   const { error } = await supabase.auth.signInWithPassword(schema);
   if (error) {
-    redirect("/error");
+    return error.message;
   }
-
-  revalidatePath("/", "layout");
-  redirect("/");
+  return null;
 }
 
-export async function signup(schema: UserSchemaType) {
+export async function signup(schema: UserSchemaType): Promise<string | null> {
   const supabase = createClient();
 
-  const { error } = await supabase.auth.signUp(schema);
+  const { data, error } = await supabase.auth.signUp(schema);
   if (error) {
-    redirect("/error");
+    return error.message;
   }
 
-  revalidatePath("/", "layout");
-  redirect("/");
+  const { error: error2 } = await supabase
+    .from("users")
+    .insert([{ userId: data.user?.id, userName: schema.data.userName }]);
+  if (error2) {
+    return error2.message;
+  }
+
+  return null;
 }
--- lib/definitions.ts
+++ lib/definitions.ts
@@ -1,30 +1,32 @@
+import { IconType } from "react-icons/lib";
 import { z } from "zod";
 
 export type Page = {
   title: string;
   href: string;
-  iconOutline: JSX.Element;
-  iconSolid: JSX.Element;
+  icon: IconType;
+  iconOutline: IconType;
 };
 
-export const UserSchema = z
-  .object({
-    email: z.string().email(),
-    password: z
+export const UserSignUpSchema = z.object({
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .max(20, "Password must be at most 20 characters"),
+  data: z.object({
+    userName: z
       .string()
-      .min(8, "Password must be at least 8 characters")
-      .max(20, "Password must be at most 20 characters"),
-    confirmPassword: z.string(),
-    data: z.object({
-      userName: z
-        .string()
-        .min(3, "Username must be at least 3 characters")
-        .max(20, "Username must be at most 20 characters"),
-      userUID: z.string(),
-    }),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: "Passwords do not match",
-    path: ["confirmPassword"],
-  });
-export type UserSchemaType = z.infer<typeof UserSchema>;
+      .regex(/^[a-zA-Z0-9_]+$/, "Username can only contain letters and numbers")
+      .min(3, "Username must be at least 3 characters")
+      .max(20, "Username must be at most 20 characters"),
+  }),
+});
+export const UserSignInSchema = z.object({
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .max(20, "Password must be at most 20 characters"),
+});
+export type UserSchemaType = z.infer<typeof UserSignUpSchema>;
